22eed4b1aa1546eaffc3e6c47dd6817c0e8f3a53
yo-wow <19966441212@126.com>
2025-10-08 11:54:24 +0800

    Multimodality多模态

--- src/main/java/com/github/yo/service/ChatModelHelper.java
+++ src/main/java/com/github/yo/service/ChatModelHelper.java
@@ -1,6 +1,8 @@
 package com.github.yo.service;
 
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,11 @@ public class ChatModelHelper {
 
     public String simpleTextChat(String message) {
         return qwenChatModel.chat(message);
+    }
+
+    public String multimodalityChat(UserMessage message) {
+        ChatResponse chatResponse = qwenChatModel.chat(message);
+
+        return chatResponse.aiMessage().text();
     }
 }
---
+++ src/test/java/com/github/yo/service/ChatModelHelperTest.java
@@ -0,0 +1,32 @@
+package com.github.yo.service;
+
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ChatModelHelperTest {
+    @Resource
+    private ChatModelHelper chatModelHelper;
+
+    @Test
+    void simpleTextChat() {
+        String resp = chatModelHelper.simpleTextChat("你好");
+        log.info("resp: {}", resp);
+    }
+
+    @Test
+    void multimodalityChat() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://avatars.githubusercontent.com/u/88282543?v=4&size=64")
+        );
+        String resp = chatModelHelper.multimodalityChat(userMessage);
+        log.info("resp: {}", resp);
+    }
+}
